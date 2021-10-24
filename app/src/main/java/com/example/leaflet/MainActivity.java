e4551ad427082a295cdb4738db7be0875a9f05a3
Shey Basil <sheykerkup5@gmail.com>
2021-10-24 16:09:29 +0100

    add hello world view, webview navigations

--- app/src/main/java/com/example/leaflet/MainActivity.java
+++ app/src/main/java/com/example/leaflet/MainActivity.java
@@ -8,29 +8,40 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.leaflet.services.web.LocalContentWebViewClient;
-import com.example.leaflet.services.web.MyWebClient;
+import com.example.leaflet.services.web.WebAppInterface;
 
 public class MainActivity extends AppCompatActivity {
-
+    WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        mWebView = findViewById(R.id.webview);
 
         final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
-                .addPathHandler("/res/", new WebViewAssetLoader.ResourcesPathHandler(this))
+                .addPathHandler(getString(R.string.assetsDir), new WebViewAssetLoader.AssetsPathHandler(this))
+                .addPathHandler(getString(R.string.resDir), new WebViewAssetLoader.ResourcesPathHandler(this))
                 .build();
 
-        WebSettings webSettings = myWebView.getSettings();
+        WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(true);
         webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
 
-        myWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader));
+        mWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader, this));
+        mWebView.addJavascriptInterface(new WebAppInterface(this, mWebView), getString(R.string.leafletInterface));
 
-        myWebView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
+        String url = getString(R.string.baseUrl).concat(getString(R.string.indexPage));
+        mWebView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()){
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/leaflet/services/web/LocalContentWebViewClient.java
+++ app/src/main/java/com/example/leaflet/services/web/LocalContentWebViewClient.java
@@ -1,5 +1,7 @@
 package com.example.leaflet.services.web;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.net.http.SslError;
 import android.webkit.SslErrorHandler;
@@ -7,18 +9,23 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.webkit.WebViewAssetLoader;
 import androidx.webkit.WebViewClientCompat;
 
+import com.example.leaflet.R;
+
 import java.net.URI;
 
 public class LocalContentWebViewClient extends WebViewClientCompat{
     private  final WebViewAssetLoader mAssetLoader;
+    private  final Context mcontext;
 
-    public LocalContentWebViewClient(WebViewAssetLoader mAssetLoader) {
+    public LocalContentWebViewClient(WebViewAssetLoader mAssetLoader, Context context) {
         this.mAssetLoader = mAssetLoader;
+        this.mcontext     = context;
     }
 
     @Override
@@ -38,5 +45,18 @@ public class LocalContentWebViewClient extends WebViewClientCompat{
     @SuppressWarnings("deprecation") // to support API < 21
     public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
         return mAssetLoader.shouldInterceptRequest(Uri.parse(url));
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(@NonNull WebView view, @NonNull WebResourceRequest request) {
+        if (mcontext.getString(R.string.baseUrl).contains(request.getUrl().getHost())) {
+            // This is my website, so do not override; let my WebView load the page
+            return false;
+        }
+        // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+        Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+        mcontext.startActivity(intent);
+        return true;
+
     }
 }
