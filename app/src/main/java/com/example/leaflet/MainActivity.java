f787717d3536507481b4d8adbb077fbfe0f29a46
Shey Basil <sheykerkup5@gmail.com>
2021-10-24 09:12:26 +0100

    enable javascript

--- app/src/main/java/com/example/leaflet/MainActivity.java
+++ app/src/main/java/com/example/leaflet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.webkit.WebViewAssetLoader;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import com.example.leaflet.services.web.LocalContentWebViewClient;
@@ -17,17 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-//        myWebView.loadUrl("https://10.12.1.172:8000/assets/index.html");
-//        myWebView.setWebViewClient(new MyWebClient());
 
         final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
                 .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
                 .addPathHandler("/res/", new WebViewAssetLoader.ResourcesPathHandler(this))
                 .build();
 
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+
         myWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader));
-//
-        myWebView.loadUrl("http://localhost:8000/assets/index.html");
-//        myWebView.loadUrl("file:///android_asset/index.html");
+
+        myWebView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
     }
 }
