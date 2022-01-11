12649db46a3738aeb61d2732160afb4ac829b07d
Shey Basil <sheykerkup5@gmail.com>
2022-01-11 08:35:47 +0100

    osmdroid caching with icons

--- app/src/main/java/com/example/leaflet/MainActivity.java
+++ app/src/main/java/com/example/leaflet/MainActivity.java
@@ -1,47 +1,117 @@
 package com.example.leaflet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.webkit.WebViewAssetLoader;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.preference.PreferenceManager;
 
-import com.example.leaflet.services.web.LocalContentWebViewClient;
-import com.example.leaflet.services.web.WebAppInterface;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    WebView mWebView;
+    private  MapView map = null;
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main);
 
-        mWebView = findViewById(R.id.webview);
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
 
-        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
-                .addPathHandler(getString(R.string.assetsDir), new WebViewAssetLoader.AssetsPathHandler(this))
-                .addPathHandler(getString(R.string.resDir), new WebViewAssetLoader.ResourcesPathHandler(this))
-                .build();
+        requestPermissionsIfNecessary(new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        });
 
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        setZoomControls();
 
-        mWebView.setWebViewClient(new LocalContentWebViewClient(assetLoader, this));
-        mWebView.addJavascriptInterface(new WebAppInterface(this, mWebView), getString(R.string.leafletInterface));
+        GeoPoint startPoint = new GeoPoint(4.04717, 9.75949);
+        setDefaultViewPoint(startPoint);
+        setMarker(startPoint);
+    }
 
-        String url = getString(R.string.baseUrl).concat(getString(R.string.mapPage));
-        mWebView.loadUrl(url);
+    private void setMarker(GeoPoint startPoint) {
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        startMarker.setTitle("Default Position");
+
+        map.getOverlays().add(startMarker);
+    }
+
+    private void setZoomControls() {
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        map.setMultiTouchControls(true);
+    }
+
+    private void setDefaultViewPoint(GeoPoint startPoint) {
+        IMapController mapController = map.getController();
+        mapController.setZoom(10.0);
+        mapController.setCenter(startPoint);
+
     }
 
     @Override
-    public void onBackPressed() {
-        if (mWebView.canGoBack()){
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>(Arrays.asList(permissions).subList(0, grantResults.length));
+
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
         }
     }
+
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+
 }
